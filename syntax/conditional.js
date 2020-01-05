7a1441ee89ebc8ad92576fb12e8317a1cdd798fe
unknown <mingyu514@naver.com>
2020-01-05 11:13:06 +0900

    first_assignment

---
+++ syntax/conditional.js
@@ -0,0 +1,11 @@
+var args = process.argv;
+console.log(args);
+console.log('A');
+console.log('B');
+if(args[2]==1){
+    console.log('C1');
+}
+else if(args[2]==2){
+    console.log('C2');
+}
+console.log('D');
